14b24cf7de0143ffdd44220daea8d4287e4dfd98
Faheem2407 <faheem24072000@gmail.com>
2025-03-15 08:17:02 +0600

    removed unnessesary lines

--- app/Enums/SectionEnum.php
+++ app/Enums/SectionEnum.php
@@ -4,14 +4,12 @@ namespace App\Enums;
 
 enum SectionEnum: string
 {
-    case SITE_INFO = 'site_info';
     case HOME_BANNER = 'home_banner';
 
     public static function HomePage()
     {
         return [
             self::HOME_BANNER->value => ['item' => 1, 'type' => 'first'],
-            self::SITE_INFO->value => ['item' => 1, 'type' => 'first'],
         ];
     }
     
--- routes/cms.php
+++ routes/cms.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Backend\Admin\CMS\SiteInfoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Admin\CMSController;
 
-Route::middleware(['auth', 'admin'])->controller(SiteInfoController::class)->prefix('cms')->group(function () {
-    Route::post('/site-info','update')->name('cms.site-info.update');
-});
-
 Route::middleware(['auth', 'admin'])->controller(CMSController::class)->prefix('cms')->group(function () {
     Route::get('/site-info','siteInfo')->name('cms.site-info');
     Route::get('/home-banner','homeBanner')->name('cms.home.banner');
+});
+
+Route::middleware(['auth', 'admin'])->controller(SiteInfoController::class)->prefix('cms')->group(function () {
+    Route::post('/site-info','update')->name('cms.site-info.update');
 });
